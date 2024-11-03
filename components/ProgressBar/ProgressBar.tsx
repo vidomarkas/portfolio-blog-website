5d9357781358838b8be4f6559b9b77edc7fa281f
Viktoras Domarkas <viktoras@bravelittletank.com>
2024-11-03 18:40:01 +0000

    move ProgressBar

--- components/ProgressBar/ProgressBar.tsx
+++ components/ProgressBar/ProgressBar.tsx
@@ -2,17 +2,16 @@
 
 import React from "react";
 import { motion, useScroll, useSpring } from "framer-motion";
+import "./ProgressBar.scss";
 
 const ProgressBar = () => {
-
-
 	const { scrollYProgress } = useScroll();
-	const scaleX = useSpring(scrollYProgress, {
+	const scaleY = useSpring(scrollYProgress, {
 		stiffness: 100,
 		damping: 30,
 		restDelta: 0.001,
 	});
-	return <motion.div className="progress-bar" style={{ scaleX }} />
+	return <motion.div className="progress-bar" style={{ scaleY }} />;
 };
 
 export default ProgressBar;
