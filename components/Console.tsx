663ef3b411743230a2bffaf602a0ed4674fccf97
Viktoras Domarkas <vidomarkas@gmail.com>
2025-01-02 02:51:39 +0000

    Console component: log only once

--- components/Console.tsx
+++ components/Console.tsx
@@ -1,10 +1,14 @@
 "use client";
+import { useEffect } from "react";
 
 const Console = () => {
-	console.log(
-		"%c The Matrix has you...  Here's the source code: https://github.com/vidomarkas/portfolio-blog-website",
-		"background: #222; color: #bada55; padding: 5px 10px; border-radius: 5px; font-size: 12px;"
-	);
+	useEffect(() => {
+		console.log(
+			"%c The Matrix has you...  Here's the source code: https://github.com/vidomarkas/portfolio-blog-website",
+			"background: #222; color: #bada55; padding: 5px 10px; border-radius: 5px; font-size: 12px;"
+		);
+	}, []);
+
 	return <></>;
 };
 
